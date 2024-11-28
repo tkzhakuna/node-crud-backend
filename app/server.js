87f7f259443ba62421428b7799327f67a468d9d0
devTatendaMarvelous <marveloustchimusoro@gmail.com>
2024-11-28 16:19:50 +0200

    Cors Fix

--- app/server.js
+++ app/server.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const app=express();
+const cors = require('cors');
 const bookRoutes=require('./routes/book.routes');
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api',bookRoutes);
